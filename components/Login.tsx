9cd00b992ea1934a2983401b53cac168f9130946
tomasherrera1910 <tomymza10@gmail.com>
2023-01-05 17:25:23 -0300

    Componetizing Login

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import api from '../utils/api'
+import InputPassword from './PasswordInput'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
 
 export function Login (): JSX.Element {
   const [passwordInput, setPasswordInput] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+
   const router = useRouter()
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>): void => {
     evt.preventDefault()
@@ -16,6 +20,7 @@ export function Login (): JSX.Element {
           : router.push('/admin')
       }).catch(e => { console.error(e) })
   }
+
   const handlePasswordInput = (evt: React.ChangeEvent<HTMLInputElement>): void => {
     setPasswordInput(evt.target.value)
     if (errorMessage !== '') { setErrorMessage('') }
@@ -24,17 +29,11 @@ export function Login (): JSX.Element {
         <div className="h-screen flex justify-center items-center flex-col gap-8 bg-slate-900 text-slate-300">
             <h1 className="text-5xl uppercase font-mono">Admin</h1>
             <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-                <label className="text-slate-400 flex flex-col gap-1">
-                    Clave
-                    <input
-                        type="password"
-                        value={passwordInput}
-                        onChange={handlePasswordInput}
-                        className="bg-slate-800 rounded py-2 px-4 outline-none focus:outline-slate-400"
-                    />
-                </label>
-                <label className="text-red-500 text-sm">{errorMessage}</label>
-                <button className="font-bold bg-slate-800 rounded py-2 text-slate-400 border-b-2 border-b-slate-400 hover:text-slate-800 hover:bg-slate-400">
+                <InputPassword passwordInput={passwordInput} handlePasswordInput={handlePasswordInput}/>
+                {errorMessage !== '' &&
+                  <label className="text-red-500 text-sm flex gap-2"><FontAwesomeIcon icon={faLock} className="w-4"/> {errorMessage}</label>
+                }
+                <button type="submit" className="font-bold bg-slate-800 rounded py-2 text-slate-400 border-b-2 border-b-slate-400 hover:text-slate-800 hover:bg-slate-400">
                     INGRESAR
                 </button>
             </form>
