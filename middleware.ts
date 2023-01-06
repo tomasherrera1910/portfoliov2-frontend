cf1a7402b74f32f73ebcfe50abfab864c68cf0e6
tomasherrera1910 <tomymza10@gmail.com>
2023-01-06 18:27:00 -0300

    if token invalid redirect /login else redirect /admin

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
-import { NextRequest, NextResponse } from "next/server";
+import api from './utils/api'
+import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest){
-    const {pathname} = req.nextUrl
-    if(pathname === '/login'){
-        const token = req.cookies.get('token')
-        if(token){
-            return NextResponse.redirect('http://localhost:3000/admin')
-        }
+export async function middleware (req: NextRequest): Promise<NextResponse> {
+  const { pathname } = req.nextUrl
+
+  if (pathname === '/login') {
+    const token = req.cookies.get('token')
+    if (token != null) {
+      const res = await api.verifyLogin(token.value)
+      const data = await res.json()
+      if (data.error === undefined && data.login === true) {
+        return NextResponse.redirect('http://localhost:3000/admin')
+      }
     }
-    return NextResponse.next()
+  } else if (pathname === '/admin') {
+    const token = req.cookies.get('token')
+    if (token != null) {
+      const res = await api.verifyLogin(token.value)
+      const data = await res.json()
+      if (typeof data.error === 'string' && data.login === undefined) {
+        return NextResponse.redirect('http://localhost:3000/login')
+      }
+    } else {
+      return NextResponse.redirect('http://localhost:3000/login')
+    }
+  }
+  return NextResponse.next()
 }
