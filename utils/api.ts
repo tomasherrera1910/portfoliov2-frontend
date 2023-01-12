03e89e567f189922c435d34322066fc5844aefca
tomasherrera1910 <tomymza10@gmail.com>
2023-01-12 15:03:22 -0300

    Using localhost for development environment

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,8 @@
-const backendURL = process.env.NEXT_PUBLIC_BACKEND_URI ?? 'http://localhost:3001'
-
+// const backendURL = process.env.NEXT_PUBLIC_BACKEND_URI ?? 'http://localhost:3001'
+const devURL = 'http://localhost:3001'
 const api = {
   login: async (password: Record<string, string>): Promise<Response> => {
-    return await fetch(`${backendURL}/login`, {
+    return await fetch(`${devURL}/login`, {
       method: 'POST',
       credentials: 'include',
       headers: {
@@ -12,7 +12,7 @@ const api = {
     })
   },
   verifyLogin: async (token: string = ''): Promise<Response> => {
-    return await fetch(`${backendURL}/verifyLogin/${token}`)
+    return await fetch(`${devURL}/verifyLogin/${token}`)
   }
 }
 export default api
