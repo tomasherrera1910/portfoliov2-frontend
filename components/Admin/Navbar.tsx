caae888c6a4cdadf978a6ebc0b0335ad95076ace
tomasherrera1910 <tomymza10@gmail.com>
2023-01-12 15:03:52 -0300

    styling navbar

--- components/Admin/Navbar.tsx
+++ components/Admin/Navbar.tsx
@@ -1,12 +1,12 @@
-import { faArrowLeft, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 export function Navbar (): JSX.Element {
   return (
-        <header className="py-4 px-2 bg-zinc-900 text-white flex justify-between items-center w-screen">
+        <header className="py-4 px-2 bg-black bg-opacity-25 backdrop-blur-sm flex justify-between items-center w-screen fixed top-0 left-0">
             <Link href={'/'} className="flex gap-2">
-                <FontAwesomeIcon icon={faArrowLeft} className="w-6"/> Portfolio
+                Ir al Portfolio
             </Link>
             <span className="flex gap-2">
                 <FontAwesomeIcon icon={faCircleUser} className="w-6"/> Admin
