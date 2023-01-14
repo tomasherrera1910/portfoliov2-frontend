8e9cd3cdc734a0726dd71b55258f5bea0cd5aef9
tomasherrera1910 <tomymza10@gmail.com>
2023-01-14 10:04:21 -0300

    Sidebar admin refactor

--- components/Admin/Sidebar.tsx
+++ components/Admin/Sidebar.tsx
@@ -1,24 +1,49 @@
 import { useToggle } from '../../hooks/useToggle'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight, faBook, faList } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faBook, faHome, faList } from '@fortawesome/free-solid-svg-icons'
+import { IButtonAside } from '../../utils/types'
+import ButtonSidebar from './ButtonSidebar'
 
 export function Sidebar (): JSX.Element {
   const { toggle, handleToggle } = useToggle()
   const asideWidth = toggle ? 'w-40' : 'w-6'
-
+  const BUTTONS: IButtonAside[] = [
+    {
+      icon: faArrowRight,
+      icon2: faArrowLeft,
+      text: '',
+      handleClick: handleToggle
+    },
+    {
+      icon: faHome,
+      icon2: null,
+      text: 'Admin Home',
+      handleClick: () => {}
+    },
+    {
+      icon: faList,
+      icon2: null,
+      text: 'Projects',
+      handleClick: () => {}
+    },
+    {
+      icon: faBook,
+      icon2: null,
+      text: 'Skills',
+      handleClick: () => {}
+    }
+  ]
+  //   const active = ' bg-white text-black'
   return (
-        <aside className={'h-screen bg-black bg-opacity-75 backdrop-blur-sm fixed top-14 left-0 flex flex-col gap-4 items-center ease-linear duration-150 ' + asideWidth}>
-            <button onClick={handleToggle}>
-                <FontAwesomeIcon icon={toggle ? faArrowLeft : faArrowRight} className="w-4"/>
-            </button>
-            <button className="flex gap-2 items-center">
-                <FontAwesomeIcon icon={faList} className="w-4"/>
-                {toggle && 'Projects'}
-            </button>
-            <button className="flex gap-2 items-center">
-                <FontAwesomeIcon icon={faBook} className="w-4"/>
-                {toggle && 'Skills'}
-            </button>
+        <aside className={'h-screen bg-black bg-opacity-75 backdrop-blur-sm fixed top-14 left-0 flex flex-col items-center ease-linear duration-150 ' + asideWidth}>
+            {BUTTONS.map((info, idx) => (
+                <ButtonSidebar
+                key={idx}
+                handleClick={info.handleClick}
+                text={info.text}
+                toggle={toggle}
+                icon={toggle ? (info.icon2 ?? info.icon) : info.icon}
+                />
+            ))}
         </aside>
   )
 }
