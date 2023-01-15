34765a5bbd700d7775171639a6da54f2f4b6375e
tomasherrera1910 <tomymza10@gmail.com>
2023-01-15 11:57:48 -0300

    Button component for iterate in Sidebar

--- components/Admin/ButtonSidebar.tsx
+++ components/Admin/ButtonSidebar.tsx
@@ -6,12 +6,13 @@ interface Props {
   text: string
   handleClick: () => void
   toggle: boolean
+  active: string
 }
 
-export default function ButtonSidebar ({ icon, text, toggle, handleClick = () => {} }: Props): JSX.Element {
-  const buttonStyle = 'flex gap-2 items-center justify-center w-full py-4 ease-linear duration-200 hover:bg-slate-600 hover:text-gray-800'
+export default function ButtonSidebar ({ icon, text, toggle, handleClick, active }: Props): JSX.Element {
+  const buttonStyle = 'flex gap-2 items-center justify-center w-full py-4 ease-linear duration-200 hover:bg-slate-800 hover:text-gray-200'
   return (
-        <button onClick={handleClick} className={buttonStyle}>
+        <button onClick={handleClick} className={`${buttonStyle} ${active}`}>
                 <FontAwesomeIcon icon={icon} className="w-4"/>
                 {toggle && text}
         </button>
