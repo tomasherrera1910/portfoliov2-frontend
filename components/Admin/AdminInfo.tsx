701bfbe92056be3963961b37114654c97f8ca7d0
tomasherrera1910 <tomymza10@gmail.com>
2023-01-15 20:42:35 -0300

    Adding admin home component

---
+++ components/Admin/AdminInfo.tsx
@@ -0,0 +1,17 @@
+export function AdminInfo (): JSX.Element {
+  return (
+    <div className="px-4 min-h-screen">
+      <h1 className="text-3xl font-bold my-4">Bienvenido!</h1>
+      <h3 className="text-xl">En este apartado vas a poder administrar/gestionar todas las Habilidades y Proyectos que se van a mostrar en tu portfolio!</h3>
+      <h2 className="text-2xl font-bold mt-4 mb-2">Podrás:</h2>
+      <ul className="list-disc pl-4 text-xl flex flex-col gap-3">
+        <li>Terminaste tu nuevo proyecto? Agregalo rapidamente a la DB a través de un formulario.</li>
+        <li>Por fin aprendiste esa tecnología nueva tan buscada por el mercado actual? Agregala a tu porfolio en simples pasos</li>
+        <li>No te convenció algo? Editalo.</li>
+        <li>Crees que algún proyecto sobra en el portfolio? Eliminalo.</li>
+        <li>También podes agregar las habilidades que usaste en tu proyecto para que posteriormente se pueda filtrar en el portfolio.</li>
+        <li>Se que quizás esto no es lo más práctico pero lo hice con fines de mantenerme activo en el desarrollo fullstack.</li>
+      </ul>
+    </div>
+  )
+}
