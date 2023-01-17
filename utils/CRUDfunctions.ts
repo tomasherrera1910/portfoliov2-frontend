5c767c9640cece892ab7ed3d184e467d70ada4cb
tomasherrera1910 <tomymza10@gmail.com>
2023-01-17 12:17:56 -0300

    get and post for reutilizate with Projects and Skills

---
+++ utils/CRUDfunctions.ts
@@ -0,0 +1,19 @@
+const devURL = 'http://localhost:3001'
+export async function getInfo (path: string): Promise<any> {
+  const response = await fetch(`${devURL}/${path}`)
+  const data = await response.json()
+  return data
+}
+
+export async function postInfo (path: string, info: any): Promise<any> {
+  const response = await fetch(`${devURL}/${path}`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify(info)
+  })
+  const data = await response.json()
+  return data
+}
