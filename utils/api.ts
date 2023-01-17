8bb00fad1fa38ac81102036cb0c1529dfbf393bc
tomasherrera1910 <tomymza10@gmail.com>
2023-01-17 12:19:12 -0300

    Adding attributes for post and get Skills and Projects

--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,6 @@
+import { getInfo, postInfo } from './CRUDfunctions'
+import { Project, Skill } from './types'
+
 // const backendURL = process.env.NEXT_PUBLIC_BACKEND_URI ?? 'http://localhost:3001'
 const devURL = 'http://localhost:3001'
 const api = {
@@ -13,6 +16,22 @@ const api = {
   },
   verifyLogin: async (token: string = ''): Promise<Response> => {
     return await fetch(`${devURL}/verifyLogin/${token}`)
+  },
+  getSkills: async (): Promise<Skill[]> => {
+    const data: Skill[] = await getInfo('skills')
+    return data
+  },
+  getProjects: async (): Promise<Project[]> => {
+    const data: Project[] = await getInfo('projects')
+    return data
+  },
+  createSkill: async (skill: Partial<Skill>): Promise<Skill> => {
+    const data: Skill = await postInfo('skills', skill)
+    return data
+  },
+  createProject: async (project: Partial<Project>): Promise<Project> => {
+    const data: Project = await postInfo('projects', project)
+    return data
   }
 }
 export default api
