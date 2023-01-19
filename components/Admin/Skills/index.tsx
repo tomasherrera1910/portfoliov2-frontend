9d0b56420822715277c7f8c72304ae109b511616
tomasherrera1910 <tomymza10@gmail.com>
2023-01-19 12:23:34 -0300

    mapping skills

--- components/Admin/Skills/index.tsx
+++ components/Admin/Skills/index.tsx
@@ -3,11 +3,13 @@ import SkillsForm from './SkillsForm'
 import { faBook, faFileCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
+import { SkillCard } from './SkillCard'
 
 interface Props {
   initialSkills: Skill[]
 }
 export function Skills ({ initialSkills }: Props): JSX.Element {
+  const [skills] = useState(initialSkills)
   const [isFormOpen, setFormOpen] = useState(false)
   const handleForm = (): void => { setFormOpen(prev => !prev) }
   return (
@@ -15,6 +17,13 @@ export function Skills ({ initialSkills }: Props): JSX.Element {
             <h1 className="text-3xl flex items-center gap-4 py-4"><FontAwesomeIcon icon={faBook} className="w-8"/> Skills</h1>
             <button onClick={handleForm} className="flex items-center gap-2 border-gray-50 rounded border-solid border-[1px] p-2 ease-in-out duration-100 hover:text-black hover:bg-zinc-200">Add Skill <FontAwesomeIcon icon={faFileCirclePlus} className="w-6"/></button>
             <SkillsForm open={isFormOpen} handleOpen={handleForm}/>
+            <section className="flex flex-wrap gap-2 py-2">
+            {skills.map(({ id, imageURL, name, technology }) => {
+              return (
+                <SkillCard key={id} imageURL={imageURL} name={name} technology={technology}/>
+              )
+            })}
+            </section>
         </section>
   )
 }
