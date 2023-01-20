762b8ed771cd526b3baac3ab2a5d5e4156553a66
tomasherrera1910 <tomymza10@gmail.com>
2023-01-20 14:55:04 -0300

    Modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  open: boolean
+  children: React.ReactNode | React.ReactNode[]
+}
+export default function Modal ({ open, children }: Props): JSX.Element {
+  return (
+        <dialog open={open} className='fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-60'>
+          {children}
+        </dialog>
+  )
+}
