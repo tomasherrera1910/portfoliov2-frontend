b6e61e94a347955d404fc561653f50c1c7e9eb9a
tomasherrera1910 <tomymza10@gmail.com>
2023-01-20 14:53:47 -0300

    Adding modal for confirm when you want delete a skill

--- components/Admin/Skills/SkillCard.tsx
+++ components/Admin/Skills/SkillCard.tsx
@@ -1,19 +1,38 @@
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import api from '../../../utils/api'
+import ModalConfirm from '../../ModalConfirm'
+import Modal from '../../Modal'
 
 interface Props {
+  id: number
   imageURL: string
   name: string
   technology: string | undefined
 }
-export function SkillCard ({ imageURL, name, technology }: Props): JSX.Element {
+export function SkillCard ({ id, imageURL, name, technology }: Props): JSX.Element {
+  const [deleteModal, setDeleteModal] = useState(false)
+  const handleDeleteModal = (): void => {
+    setDeleteModal(prevState => !prevState)
+  }
+  const handleDelete = (): void => {
+    api.deleteSkill(id)
+      .finally(() => {
+        handleDeleteModal()
+      })
+  }
   return (
+    <>
     <article className='border-[1px] border-zinc-100 rounded p-2 bg-black bg-opacity-50'>
         <img src={imageURL} alt={name}/>
         <h1>{name}</h1>
         <span>{technology}</span>
         <div className='flex justify-around'>
-            <button className='bg-red-900 p-1 rounded ease-in-out duration-200 hover:text-red-900 hover:bg-white'>
+            <button
+            className='bg-red-900 p-1 rounded ease-in-out duration-200 hover:text-red-900 hover:bg-white'
+            onClick={handleDeleteModal}
+            >
                 <FontAwesomeIcon icon={faTrash} className='w-3'/>
             </button>
             <button className='bg-blue-900 p-1 rounded ease-in-out duration-200 hover:text-blue-900 hover:bg-white'>
@@ -21,5 +40,11 @@ export function SkillCard ({ imageURL, name, technology }: Props): JSX.Element {
             </button>
         </div>
     </article>
+    <Modal open={deleteModal}>
+      <ModalConfirm handleClose={handleDeleteModal} handleConfirm={handleDelete}>
+        <h1>Want you delete the Skill {name} (ID: {id})</h1>
+      </ModalConfirm>
+    </Modal>
+    </>
   )
 }
