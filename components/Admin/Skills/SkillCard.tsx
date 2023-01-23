6aba1c72011d1d8d09cbae93aff36da4df1d695b
tomasherrera1910 <tomymza10@gmail.com>
2023-01-23 14:38:59 -0300

    Add styles and funcionality to the edit button

--- components/Admin/Skills/SkillCard.tsx
+++ components/Admin/Skills/SkillCard.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import api from '../../../utils/api'
 import ModalConfirm from '../../ModalConfirm'
 import Modal from '../../Modal'
+import SkillsForm from './SkillsForm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
   id: number
@@ -14,8 +15,14 @@ interface Props {
 }
 export function SkillCard ({ id, imageURL, name, technology, update }: Props): JSX.Element {
   const [deleteModal, setDeleteModal] = useState(false)
+  const [editModal, setEditModal] = useState(false)
+  const [colorImage, setColorImage] = useState('grayscale')
+  const actionsButtons = 'p-1 rounded ease-in-out duration-200 hover:bg-white'
   const handleDeleteModal = (): void => {
     setDeleteModal(prevState => !prevState)
+  }
+  const handleEditModal = (): void => {
+    setEditModal(prevState => !prevState)
   }
   const handleDelete = (): void => {
     api.deleteSkill(id)
@@ -28,18 +35,25 @@ export function SkillCard ({ id, imageURL, name, technology, update }: Props): J
   }
   return (
     <>
-    <article className='border-[1px] border-zinc-100 rounded p-2 bg-black bg-opacity-50'>
-        <img src={imageURL} alt={name}/>
-        <h1>{name}</h1>
-        <span>{technology}</span>
-        <div className='flex justify-around'>
+    <article
+    className='border-[1px] border-zinc-100 rounded p-2 bg-black bg-opacity-50 flex flex-col items-center justify-center'
+    onMouseEnter={() => { setColorImage('grayscale-0') }}
+    onMouseLeave={() => { setColorImage('grayscale') }}
+    >
+        <img src={imageURL} alt={name} className={`w-16 ${colorImage} ease-in-out duration-200`}/>
+        <h1 className="font-bold">{name}</h1>
+        <span className='text-gray-200 font-extralight text-xs'>{technology}</span>
+        <div className='flex justify-around pt-2 w-full'>
             <button
-            className='bg-red-900 p-1 rounded ease-in-out duration-200 hover:text-red-900 hover:bg-white'
+            className={`${actionsButtons} bg-red-900 hover:text-red-900`}
             onClick={handleDeleteModal}
             >
                 <FontAwesomeIcon icon={faTrash} className='w-3'/>
             </button>
-            <button className='bg-blue-900 p-1 rounded ease-in-out duration-200 hover:text-blue-900 hover:bg-white'>
+            <button
+            className={`${actionsButtons} bg-blue-900 hover:text-blue-900`}
+            onClick={handleEditModal}
+            >
                 <FontAwesomeIcon icon={faPenToSquare} className='w-3'/>
             </button>
         </div>
@@ -49,6 +63,7 @@ export function SkillCard ({ id, imageURL, name, technology, update }: Props): J
         <h1>Want you delete the Skill {name} (ID: {id})</h1>
       </ModalConfirm>
     </Modal>
+    <SkillsForm open={editModal} handleOpen={handleEditModal} update={update} edit INITIAL_FORM={{ id, name, imageURL, technology }}/>
     </>
   )
 }
