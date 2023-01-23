bf24626e169908a8a93d4de712a5b089e7bf8e6d
tomasherrera1910 <tomymza10@gmail.com>
2023-01-23 14:38:28 -0300

    add more props for the edit form

--- components/Admin/Skills/SkillsForm.tsx
+++ components/Admin/Skills/SkillsForm.tsx
@@ -1,17 +1,19 @@
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import useForm from '../../../hooks/useForm'
-import api from '../../../utils/api'
+import { Skill } from '../../../utils/types'
 import Modal from '../../Modal'
 
 interface Props {
   open: boolean
+  edit?: boolean
+  INITIAL_FORM?: Partial<Skill>
   handleOpen: () => void
   update: () => Promise<void>
 }
 
-export default function SkillsForm ({ open, handleOpen, update }: Props): JSX.Element {
-  const { errorMessage, handleChange, handleSubmit, form } = useForm(handleOpen, update)
+export default function SkillsForm ({ open, handleOpen, update, edit, INITIAL_FORM }: Props): JSX.Element {
+  const { errorMessage, handleChange, handleSubmit, form } = useForm({ edit, INITIAL_FORM, handleOpen, update })
   const inputStyle = 'bg-gray-900 text-white p-1 outline-none rounded ease-in-out duration-150 focus:outline-1 focus:outline-cyan-400 focus:shadow-sm focus:shadow-cyan-600'
   const labelStyle = 'flex flex-col gap-1 text-gray-300 text-sm'
   const displayModal = open ? 'block' : 'hidden'
@@ -21,8 +23,8 @@ export default function SkillsForm ({ open, handleOpen, update }: Props): JSX.El
             <button className="absolute top-2 right-2 ease-in-out duration-150 text-white hover:text-red-900" onClick={handleOpen}>
               <FontAwesomeIcon icon={faClose} className="w-4"/>
             </button>
-            <h1 className="font-bold text-white">ADD SKILL</h1>
-            <form onSubmit={(evt) => { handleSubmit(evt, api.createSkill) }} className={'flex flex-col gap-2 my-2 justify-center'}>
+            <h1 className="font-bold text-white">{(edit ?? false) ? 'EDIT' : 'ADD'} SKILL</h1>
+            <form onSubmit={(evt) => { handleSubmit(evt) }} className={'flex flex-col gap-2 my-2 justify-center'}>
                 <label className={labelStyle}>
                     Name *
                     <input value={form.name} onChange={(evt) => { handleChange(evt, 'name') }} className={`${inputStyle}`}/>
@@ -37,7 +39,7 @@ export default function SkillsForm ({ open, handleOpen, update }: Props): JSX.El
                 </label>
                 {errorMessage !== '' && <span className="text-xs">{errorMessage}</span>}
                 <button className='text-white border-[1px] border-cyan-600 rounded bg-gray-900 ease-in-out duration-200 hover:border-white hover:text-cyan-600'>
-                  ADD SKILL
+                {(edit ?? false) ? 'EDIT' : 'ADD'} SKILL
                 </button>
             </form>
           </div>
