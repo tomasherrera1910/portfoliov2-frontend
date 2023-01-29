5f49d30f7ac4713d5679ae9427cdcb23d8850574
tomasherrera1910 <tomymza10@gmail.com>
2023-01-29 16:23:07 -0300

    change admin bg

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -31,7 +31,7 @@ const AdminPage: NextPage<Props> = ({ skills, projects }: Props) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className='min-h-screen bg-gradient-to-tr from-neutral-900 via-slate-900 to-indigo-900 text-white pr-4 pt-16 pl-12 min-[600px]:pl-16 scroll-smooth'>
+            <main className='min-h-screen bg-neutral-900 text-white pr-4 pt-16 pl-12 min-[600px]:pl-16 scroll-smooth'>
                 <Navbar />
                 <Sidebar />
                 <AdminInfo />
