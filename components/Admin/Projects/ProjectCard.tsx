4daa9028df91f4869a8d15b09c9ffb56bb23e4ab
tomasherrera1910 <tomymza10@gmail.com>
2023-01-30 17:42:58 -0300

    Adding styles to project card images

--- components/Admin/Projects/ProjectCard.tsx
+++ components/Admin/Projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Project } from '../../../utils/types'
 
 interface Props {
@@ -5,9 +6,46 @@ interface Props {
 }
 
 export function ProjectCard ({ project }: Props): JSX.Element {
+  const [isMobileHover, setMobileHover] = useState('')
+  const [isDesktopHover, setDesktopHover] = useState('')
+  const baseImageStyle = 'absolute opacity-80 border-stone-200 border-[1px] rounded ease-in-out duration-200'
+  const handleMobileHover = (): void => {
+    setMobileHover(prevState => {
+      if (prevState === '') {
+        return 'z-10 opacity-100 scale-110'
+      } else {
+        return ''
+      }
+    })
+  }
+  const handleDesktopHover = (): void => {
+    setDesktopHover(prevState => {
+      if (prevState === '') {
+        return 'z-10 opacity-100 scale-110'
+      } else {
+        return ''
+      }
+    })
+  }
   return (
-        <article>
-            {project.name}
+        <article className='relative flex w-48 h-48'>
+            <img
+            id={`mobile-${project.name}`}
+            src={project.images.mobile}
+            alt={`Mobile Image from project: ${project.name}`}
+            className={`${baseImageStyle} top-0 left-0 h-48 ${isMobileHover}`}
+            onMouseEnter={handleMobileHover}
+            onMouseLeave={handleMobileHover}
+            />
+            <img
+            id={`desktop-${project.name}`}
+            src={project.images.desktop}
+            alt={`Desktop Image from project: ${project.name}`}
+            className={`${baseImageStyle} -bottom-3 left-6 w-48 ${isDesktopHover}`}
+            onMouseEnter={handleDesktopHover}
+            onMouseLeave={handleDesktopHover}
+            />
+            <h1>{project.name}</h1>
         </article>
   )
 }
