fa993ab431096c153842e87b0524128ef9a23980
tomasherrera1910 <tomymza10@gmail.com>
2023-01-30 17:42:38 -0300

    z-index for navbar

--- components/Admin/Navbar.tsx
+++ components/Admin/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export function Navbar (): JSX.Element {
   return (
-        <header className="py-4 px-4 bg-black bg-opacity-25 backdrop-blur-sm flex justify-between items-center w-full fixed top-0 left-0 border-b-[1px] border-gray-500">
+        <header className="py-4 px-4 bg-black bg-opacity-25 backdrop-blur-sm flex justify-between items-center w-full fixed top-0 left-0 border-b-[1px] border-gray-500 z-[5]">
             <Link href={'/'} className="flex gap-2 ease-in-out duration-100 hover:scale-110 hover:font-bold">
                 Ir al Portfolio
             </Link>
