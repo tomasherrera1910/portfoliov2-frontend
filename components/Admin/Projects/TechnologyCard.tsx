c668cd951bdeb9f52633abb3186c4c0948ee2052
tomasherrera1910 <tomymza10@gmail.com>
2023-02-02 17:13:09 -0300

    Technology card and form styles

--- components/Admin/Projects/TechnologyCard.tsx
+++ components/Admin/Projects/TechnologyCard.tsx
@@ -1,3 +1,5 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Skill } from '../../../utils/types'
 
 interface Props {
@@ -5,8 +7,12 @@ interface Props {
 }
 export function TechnologyCard ({ technology }: Props): JSX.Element {
   return (
-        <article>
-            {technology.name}
-        </article>
+    <li className='flex gap-1 items-center'>
+      <img src={technology.imageURL} alt={`${technology.name ?? ''} logo`} className='w-3 h-3 object-cover'/>
+      <span className='text-xs'>{technology.name}</span>
+      <button className='p-1 bg-red-700 rounded ease-in-out duration-200 hover:bg-red-900'>
+        <FontAwesomeIcon icon={faTrash} className='w-2'/>
+      </button>
+    </li>
   )
 }
--- components/Admin/Projects/TechnologyForm.tsx
+++ components/Admin/Projects/TechnologyForm.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 export function TechnologyForm ({ id, skills, update }: Props): JSX.Element {
   const [selected, setSelected] = useState('')
-  const InputButtonStyle = 'bg-neutral-900 border-[1px] border-gray-500 rounded p-1 ease-in-out duration-200 hover:saturate-150 hover:scale-105'
+  const InputButtonStyle = 'bg-neutral-900 border-[1px] border-gray-500 rounded ease-in-out duration-200 hover:saturate-150 hover:scale-105'
   const handleSelected = (evt: React.ChangeEvent<HTMLSelectElement>): void => {
     setSelected(evt.target.value)
   }
@@ -23,16 +23,16 @@ export function TechnologyForm ({ id, skills, update }: Props): JSX.Element {
       })
   }
   return (
-        <form onSubmit={handleSubmit} className='flex gap-2'>
-         <select className={`${InputButtonStyle} cursor-pointer`} onChange={handleSelected}>
+        <form onSubmit={handleSubmit} className='flex gap-2 py-1'>
+         <select className={`${InputButtonStyle} cursor-pointer text-xs`} onChange={handleSelected}>
             {skills.map(skill => (
                 <option key={skill.id} value={skill.id}>
                     {skill.name}
                 </option>
             ))}
          </select>
-         <button type='submit' className={InputButtonStyle}>
-            <FontAwesomeIcon icon={faAdd} className='w-4'/>
+         <button type='submit' className={`${InputButtonStyle} p-1`}>
+            <FontAwesomeIcon icon={faAdd} className='w-2'/>
          </button>
         </form>
   )
