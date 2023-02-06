0e7b0cc52e638a358318393d3950d75920c83028
tomasherrera1910 <tomymza10@gmail.com>
2023-02-06 18:37:26 -0300

    edit and delete project

--- components/Admin/Projects/ProjectCard.tsx
+++ components/Admin/Projects/ProjectCard.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link'
 import { Project, Skill } from '../../../utils/types'
 import { TechnologyCard } from './TechnologyCard'
 import { TechnologyForm } from './TechnologyForm'
+import ProjectsForm from './ProjectsForm'
+import Modal from '../../Modal'
+import ModalConfirm from '../../ModalConfirm'
+import api from '../../../utils/api'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
@@ -15,7 +19,22 @@ interface Props {
 export function ProjectCard ({ project, skills, update }: Props): JSX.Element {
   const [isMobileHover, setMobileHover] = useState('')
   const [isDesktopHover, setDesktopHover] = useState('')
+  const [deleteModal, setDeleteModal] = useState(false)
+  const [editModal, setEditModal] = useState(false)
   const baseImageStyle = 'absolute border-stone-200 border-[1px] rounded ease-in-out duration-200'
+
+  const handleDeleteModal = (): void => { setDeleteModal(!deleteModal) }
+  const handleEditModal = (): void => { setEditModal(!editModal) }
+  const handleDelete = (): void => {
+    api.deleteProject(project.id)
+      .then(async () => {
+        await update()
+      })
+      .finally(() => {
+        handleDeleteModal()
+      })
+  }
+
   const handleMobileHover = (): void => {
     setMobileHover(prevState => {
       if (prevState === '') {
@@ -35,69 +54,83 @@ export function ProjectCard ({ project, skills, update }: Props): JSX.Element {
     })
   }
   return (
-        <article className={'flex flex-col gap-4 py-2'}>
-          <section className='flex relative'>
-           <div className='w-28 h-48'>
-              <img
-              id={`mobile-${project.name}`}
-              src={project.images.mobile}
-              alt={`Mobile Image from project: ${project.name}`}
-              className={`${baseImageStyle} top-0 left-0 h-48 w-24 object-cover ${isMobileHover}`}
-              onMouseEnter={handleMobileHover}
-              onMouseLeave={handleMobileHover}
-              />
-              <img
-              id={`desktop-${project.name}`}
-              src={project.images.desktop}
-              alt={`Desktop Image from project: ${project.name}`}
-              className={`${baseImageStyle} -bottom-3 left-6 w-48 h-28 object-cover ${isDesktopHover}`}
-              onMouseEnter={handleDesktopHover}
-              onMouseLeave={handleDesktopHover}
-              />
-           </div>
-          <h1 className='text-2xl font-bold max-w-[12rem]'>{project.name}</h1>
-          </section>
-          <section>
-            <p className='text-sm text-slate-300 max-w-xs'>{project.description}</p>
-            <label className='flex items-center text-xs text-slate-300 gap-1'>
-              Deploy URL:
-            <Link href={project.deployURL} className='text-cyan-500 text-base hover:underline'>{project.deployURL}</Link>
-            </label>
-            {
-            (project.backendRepo != null) &&
-            <label className='flex items-center text-xs text-slate-300 gap-1'>
-              Backend Repo:
-            <Link href={project.backendRepo} className='text-cyan-500 text-base hover:underline'>{project.backendRepo}</Link>
-            </label>
-            }
-            {
-            (project.frontEndRepo != null) &&
-            <label className='flex items-center text-xs text-slate-300 gap-1'>
-              Frontend Repo:
-            <Link href={project.frontEndRepo} className='text-cyan-500 text-base hover:underline'>{project.frontEndRepo}</Link>
-            </label>
-            }
-            {
-              project.technologies.length > 0 &&
-              <>
-              <h3 className='font-bold'>Technologies</h3>
-              {project.technologies.map(technology => (
-                <TechnologyCard key={technology.name} technology={technology} update={update}/>
-              ))}
-              </>
-            }
-            <TechnologyForm id={project.id} skills={skills} update={update}/>
-            <div className='flex gap-2'>
-              <button className={'bg-red-700 flex gap-1 text-sm p-1 rounded duration-300 ease-in-out hover:bg-red-900'}>
-                Delete Project
-                <FontAwesomeIcon icon={faTrash} className='w-4'/>
-              </button>
-              <button className={'bg-blue-700 flex gap-1 text-sm p-1 rounded duration-300 ease-in-out hover:bg-blue-900'}>
-                Edit Project
-                <FontAwesomeIcon icon={faPenToSquare} className='w-4'/>
-              </button>
-            </div>
-          </section>
-        </article>
+    <>
+      <article className={'flex flex-col gap-4 py-2'}>
+        <section className='flex relative'>
+          <div className='w-28 h-48'>
+            <img
+            id={`mobile-${project.name}`}
+            src={project.images.mobile}
+            alt={`Mobile Image from project: ${project.name}`}
+            className={`${baseImageStyle} top-0 left-0 h-48 w-24 object-cover ${isMobileHover}`}
+            onMouseEnter={handleMobileHover}
+            onMouseLeave={handleMobileHover}
+            />
+            <img
+            id={`desktop-${project.name}`}
+            src={project.images.desktop}
+            alt={`Desktop Image from project: ${project.name}`}
+            className={`${baseImageStyle} -bottom-3 left-6 w-48 h-28 object-cover ${isDesktopHover}`}
+            onMouseEnter={handleDesktopHover}
+            onMouseLeave={handleDesktopHover}
+            />
+          </div>
+        <h1 className='text-2xl font-bold max-w-[12rem]'>{project.name}</h1>
+        </section>
+        <section>
+          <p className='text-sm text-slate-300 max-w-xs'>{project.description}</p>
+          <label className='flex items-center text-xs text-slate-300 gap-1'>
+            Deploy URL:
+          <Link href={project.deployURL} className='text-cyan-500 text-base hover:underline'>{project.deployURL}</Link>
+          </label>
+          {
+          (project.backendRepo != null) &&
+          <label className='flex items-center text-xs text-slate-300 gap-1'>
+            Backend Repo:
+          <Link href={project.backendRepo} className='text-cyan-500 text-base hover:underline'>{project.backendRepo}</Link>
+          </label>
+          }
+          {
+          (project.frontEndRepo != null) &&
+          <label className='flex items-center text-xs text-slate-300 gap-1'>
+            Frontend Repo:
+          <Link href={project.frontEndRepo} className='text-cyan-500 text-base hover:underline'>{project.frontEndRepo}</Link>
+          </label>
+          }
+          {
+            project.technologies.length > 0 &&
+            <>
+            <h3 className='font-bold'>Technologies</h3>
+            {project.technologies.map(technology => (
+              <TechnologyCard key={technology.name} technology={technology} update={update}/>
+            ))}
+            </>
+          }
+          <TechnologyForm id={project.id} skills={skills} update={update}/>
+          <div className='flex gap-2'>
+            <button onClick={handleDeleteModal} className={'bg-red-700 flex gap-1 text-sm p-1 rounded duration-300 ease-in-out hover:bg-red-900'}>
+              Delete Project
+              <FontAwesomeIcon icon={faTrash} className='w-4'/>
+            </button>
+            <button onClick={handleEditModal} className={'bg-blue-700 flex gap-1 text-sm p-1 rounded duration-300 ease-in-out hover:bg-blue-900'}>
+              Edit Project
+              <FontAwesomeIcon icon={faPenToSquare} className='w-4'/>
+            </button>
+          </div>
+        </section>
+      </article>
+      <Modal open={deleteModal}>
+      <ModalConfirm handleClose={handleDeleteModal} handleConfirm={handleDelete}>
+        <h1>Want you delete the Skill {project.name} (ID: {project.id})</h1>
+      </ModalConfirm>
+    </Modal>
+      <ProjectsForm
+      open={editModal}
+      handleOpen={handleEditModal}
+      update={update}
+      edit
+      INITIAL_FORM={project}
+      />
+    </>
   )
 }
