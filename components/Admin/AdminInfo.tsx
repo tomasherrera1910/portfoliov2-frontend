0aeeed399eaea74c7a02e8aead7735928000e552
tomasherrera1910 <tomymza10@gmail.com>
2023-02-13 18:51:50 -0300

    id for navigate in adminInfo and adding padding than i remove from Admin main

--- components/Admin/AdminInfo.tsx
+++ components/Admin/AdminInfo.tsx
@@ -3,7 +3,7 @@ import { forwardRef, MutableRefObject } from 'react'
 // eslint-disable-next-line react/display-name
 export const AdminInfo = forwardRef((props, ref): JSX.Element => {
   return (
-    <div ref={ref as MutableRefObject<HTMLDivElement>}>
+    <div ref={ref as MutableRefObject<HTMLDivElement>} id='adminInfo' className='pt-16'>
       <h1 className="text-3xl font-bold my-4">Bienvenido!</h1>
       <h3 className="text-xl">En este apartado vas a poder administrar/gestionar todas las Habilidades y Proyectos que se van a mostrar en tu portfolio!</h3>
       <h2 className="text-2xl font-bold mt-4 mb-2">Podrás:</h2>
