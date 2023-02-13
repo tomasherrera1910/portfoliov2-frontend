6b072406ef4efee6f32fdb2eae042e115d9a4d8a
tomasherrera1910 <tomymza10@gmail.com>
2023-02-13 18:50:08 -0300

    pass new prop for know if component is a button or anchor

--- components/Admin/Sidebar.tsx
+++ components/Admin/Sidebar.tsx
@@ -7,7 +7,7 @@ export function Sidebar ({ adminInfoHeight }: Props): JSX.Element {
   const { BUTTONS, activeButton, asideWidth, toggle } = useSidebar()
 
   return (
-        <aside className={'h-screen bg-black bg-opacity-25 backdrop-blur-sm fixed top-14 left-0 border-r-[1px] z-10 border-gray-500 flex flex-col items-center ease-linear duration-150 ' + asideWidth}>
+        <aside className={'h-screen bg-black bg-opacity-25 backdrop-blur-sm fixed top-14 left-0 border-r-[1px] z-10 border-gray-500 flex flex-col items-center ease-linear duration-150 px-4 ' + asideWidth}>
             {BUTTONS.map((info, idx) => (
                 <ButtonSidebar
                 key={idx}
@@ -16,6 +16,7 @@ export function Sidebar ({ adminInfoHeight }: Props): JSX.Element {
                 toggle={toggle}
                 active={activeButton === info.text ? 'bg-white text-black' : ''}
                 icon={toggle ? (info.icon2 ?? info.icon) : info.icon}
+                nav={info.nav}
                 />
             ))}
         </aside>
