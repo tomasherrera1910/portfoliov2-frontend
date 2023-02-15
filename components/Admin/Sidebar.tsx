6129743b538b9a5d7d6f4eaac32c56b6122563b0
tomasherrera1910 <tomymza10@gmail.com>
2023-02-15 20:27:35 -0300

    new Props

--- components/Admin/Sidebar.tsx
+++ components/Admin/Sidebar.tsx
@@ -1,11 +1,12 @@
 import useSidebar from '../../hooks/useSidebar'
 import ButtonSidebar from './ButtonSidebar'
 interface Props {
-  adminInfoHeight?: number
+  adminInfoHeight: number
+  projectsHeight: number
+  skillsHeight: number
 }
-export function Sidebar ({ adminInfoHeight }: Props): JSX.Element {
-  const { BUTTONS, activeButton, asideWidth, toggle } = useSidebar()
-
+export function Sidebar ({ adminInfoHeight, projectsHeight, skillsHeight }: Props): JSX.Element {
+  const { BUTTONS, activeButton, asideWidth, toggle } = useSidebar(adminInfoHeight, projectsHeight, skillsHeight)
   return (
         <aside className={'h-screen bg-black bg-opacity-25 backdrop-blur-sm fixed top-14 left-0 border-r-[1px] z-10 border-gray-500 flex flex-col items-center ease-linear duration-150 px-4 ' + asideWidth}>
             {BUTTONS.map((info, idx) => (
