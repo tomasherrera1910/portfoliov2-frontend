ee2bcda05443e1608e0477657b5e3c867be42b4d
tomasherrera1910 <tomymza10@gmail.com>
2023-02-20 17:07:19 -0300

    receiving values from props

--- components/Admin/Sidebar.tsx
+++ components/Admin/Sidebar.tsx
@@ -1,22 +1,20 @@
-import useSidebar from '../../hooks/useSidebar'
+
+import { SidebarProps } from '../../hooks/useSidebar'
 import ButtonSidebar from './ButtonSidebar'
-interface Props {
-  adminInfoHeight: number
-  projectsHeight: number
-  skillsHeight: number
-}
-export function Sidebar ({ adminInfoHeight, projectsHeight, skillsHeight }: Props): JSX.Element {
-  const { BUTTONS, activeButton, asideWidth, toggle } = useSidebar(adminInfoHeight, projectsHeight, skillsHeight)
+
+export function Sidebar ({ adminHeight, projectsHeight, skillsHeight, BUTTONS, activeButton, toggle }: Partial<SidebarProps>): JSX.Element {
+  const asideWidth = toggle ?? false ? 'w-40' : 'w-6 min-[600px]:w-12'
+
   return (
         <aside className={'h-screen bg-black bg-opacity-25 backdrop-blur-sm fixed top-14 left-0 border-r-[1px] z-10 border-gray-500 flex flex-col items-center ease-linear duration-150 px-4 ' + asideWidth}>
-            {BUTTONS.map((info, idx) => (
+            {BUTTONS?.map((info, idx) => (
                 <ButtonSidebar
                 key={idx}
                 handleClick={info.handleClick}
                 text={info.text}
-                toggle={toggle}
+                toggle={toggle ?? false}
                 active={activeButton === info.text ? 'bg-white text-black' : ''}
-                icon={toggle ? (info.icon2 ?? info.icon) : info.icon}
+                icon={toggle ?? false ? (info.icon2 ?? info.icon) : info.icon}
                 nav={info.nav}
                 />
             ))}
