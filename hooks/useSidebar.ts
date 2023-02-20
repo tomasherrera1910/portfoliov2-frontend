57f45d87c2ab7bba609af09a276c34065ffdc110
tomasherrera1910 <tomymza10@gmail.com>
2023-02-20 17:07:45 -0300

    export interface and some refactors

--- hooks/useSidebar.ts
+++ hooks/useSidebar.ts
@@ -4,10 +4,9 @@ import { getBUTTONS } from '../utils/sidebarAdminButtons'
 import { IButtonAside } from '../utils/types'
 import useHeightRef from './useHeightRef'
 
-interface returnHook {
+export interface SidebarProps {
   BUTTONS: IButtonAside[]
   activeButton: string
-  asideWidth: string
   toggle: boolean
   adminHeight: number
   projectsHeight: number
@@ -16,11 +15,11 @@ interface returnHook {
   projectsRef: MutableRefObject<HTMLDivElement | undefined>
   skillsRef: MutableRefObject<HTMLDivElement | undefined>
 }
-const useSidebar = (): returnHook => {
+const useSidebar = (): SidebarProps => {
   const { toggle, handleToggle } = useToggle()
   const { adminHeight, projectsHeight, skillsHeight, adminInfoRef, projectsRef, skillsRef } = useHeightRef()
   const [activeButton, setActiveButton] = useState('INITIAL_TEXT')
-  const asideWidth = toggle ? 'w-40' : 'w-6 min-[600px]:w-12'
+
   const handleActive = (buttonText: string): void => {
     setActiveButton(buttonText)
   }
@@ -38,7 +37,7 @@ const useSidebar = (): returnHook => {
     }
   }, [])
   const BUTTONS = getBUTTONS(handleToggle, handleActive)
-  return { BUTTONS, activeButton, asideWidth, toggle, adminHeight, projectsHeight, skillsHeight, adminInfoRef, projectsRef, skillsRef }
+  return { BUTTONS, activeButton, toggle, adminHeight, projectsHeight, skillsHeight, adminInfoRef, projectsRef, skillsRef }
 }
 
 export default useSidebar
