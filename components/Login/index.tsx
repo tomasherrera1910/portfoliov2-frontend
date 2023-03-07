1fb3d4e68800f8918e308ee28584cdca8af3b84a
tomasherrera1910 <tomymza10@gmail.com>
2023-03-07 18:08:24 -0300

    Moving  adming login to new folder

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import api from '../utils/api'
+import api from '../../utils/api'
 import InputPassword from './PasswordInput'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock } from '@fortawesome/free-solid-svg-icons'
