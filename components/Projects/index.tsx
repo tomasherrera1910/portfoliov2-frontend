c854b30caf678fdb1607cfdfd2be3598945babb7
tomasherrera1910 <tomymza10@gmail.com>
2023-03-17 18:36:44 -0300

    projects section responsive

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -10,9 +10,9 @@ interface Props {
 export function Projects ({ projects, skills }: Props): JSX.Element {
   const { filteredProjects, filterToggle, resetFilters, filters } = useFiltersProjects({ initialProjects: projects })
   return (
-    <section className='w-full flex gap-4 justify-center'>
+    <section className='w-full flex flex-col lg:flex-row gap-4 justify-center'>
         <ProjectsFilter skills={skills} filterToggle={filterToggle} resetFilters={resetFilters} results={filteredProjects.length} filters={filters}/>
-        <section className='flex flex-col gap-2 grow max-w-[720px]'>
+        <section className='flex flex-col gap-2 grow lg:max-w-[720px]'>
         {
             filteredProjects.map((project) => {
               return <ProjectCard key={project.id} project={project}/>
