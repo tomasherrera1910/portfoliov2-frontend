f4ce6060d7717a7da21591e3a4bf24e81f8ba7dc
tomasherrera1910 <tomymza10@gmail.com>
2023-03-18 23:06:37 -0300

    responsive

--- components/Projects/ProjectCardLinks.tsx
+++ components/Projects/ProjectCardLinks.tsx
@@ -13,19 +13,19 @@ interface Props {
 }
 export default function ProjectCardLinks ({ backendRepo, frontEndRepo, deployURL, primary, secondary }: Props): JSX.Element {
   return (
-        <footer className='flex justify-around grow items-center'>
+        <footer className='flex flex-col gap-2 justify-around items-center md:flex-row lg:grow'>
               {(backendRepo != null) &&
-              <Link href={backendRepo} className='flex gap-2 items-center rounded p-1 bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
+              <Link href={backendRepo} className='flex gap-2 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
                 <span>Backend</span>
                 <FontAwesomeIcon icon={faGithub as IconProp} width={20}/>
               </Link>}
               {(frontEndRepo != null) &&
-              <Link href={frontEndRepo} className='flex gap-2 items-center rounded p-1 bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
+              <Link href={frontEndRepo} className='flex gap-2 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
                 <span>Frontend</span>
                 <FontAwesomeIcon icon={faGithub as IconProp} width={20}/>
               </Link>}
               {(deployURL != null) &&
-              <Link href={deployURL} className='flex gap-2 items-center rounded p-1 bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
+              <Link href={deployURL} className='flex gap-2 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105' style={{ background: `radial-gradient(circle, ${primary} 0%, ${secondary} 100%)` }}>
                 <span>Deploy</span>
                 <FontAwesomeIcon icon={faLink} width={20}/>
               </Link>}
