1a3bcb2ba1fe211029b35d9c7c0d322de2fe545f
tomasherrera1910 <tomymza10@gmail.com>
2023-03-20 19:40:11 -0300

    aside and finishing mobile styles for contact section

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import ContactInfo from './ContactInfo'
 import ContactInput from './ContactInput'
 
 const INITIAL_FORM = {
@@ -16,15 +17,20 @@ export function Contact (): JSX.Element {
     })
   }
   return (
-      <section className='flex flex-col gap-2 my-8'>
-        <h3>CONTACTO</h3>
-        <form>
+      <section className='flex flex-col my-8'>
+        <form className='bg-black bg-opacity-40 rounded-tr-md p-2'>
+          <h3 className='text-sky-900 font-bold text-2xl text-center'>CONTACTAME</h3>
           <ContactInput placeholder={'Nombre'} inputValue={inputsFields.name} handleChange={(value: string) => { handleChangeInput('name', value) }}/>
           <ContactInput placeholder={'Email'} inputValue={inputsFields.email} handleChange={(value: string) => { handleChangeInput('email', value) }}/>
           <ContactInput placeholder={'Asunto'} inputValue={inputsFields.subject} handleChange={(value: string) => { handleChangeInput('subject', value) }}/>
           <ContactInput area placeholder={'Mensaje'} inputValue={inputsFields.message} handleChange={(value: string) => { handleChangeInput('message', value) }}/>
-          <button>ENVIAR</button>
+          <button className='bg-black bg-opacity-70 w-full py-2 rounded-sm border-b-2 border-b-slate-400 text-slate-400 ease-linear duration-200 hover:text-sky-900 hover:border-b-sky-900 hover:scale-95'>
+            ENVIAR
+          </button>
         </form>
+        <aside className='text-slate-200 font-bold bg-gradient-to-b from-slate-900 via-sky-900 to-sky-800 bg-opacity-20 flex justify-around flex-wrap p-1 gap-2 rounded-bl-md'>
+          <ContactInfo/>
+        </aside>
       </section>
   )
 }
