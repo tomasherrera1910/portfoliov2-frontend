fb9e157d2575d2cfacdaf4094b6d337f631fc05f
tomasherrera1910 <tomymza10@gmail.com>
2023-03-20 19:50:31 -0300

    contact responsive

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -17,8 +17,8 @@ export function Contact (): JSX.Element {
     })
   }
   return (
-      <section className='flex flex-col my-8'>
-        <form className='bg-black bg-opacity-40 rounded-tr-md p-2'>
+      <section className='flex flex-col sm:flex-row my-8 sm:justify-center'>
+        <form className='bg-black bg-opacity-40 rounded-tr-md p-2 sm:grow sm:rounded-bl-md sm:rounded-tr-none max-w-sm'>
           <h3 className='text-sky-900 font-bold text-2xl text-center'>CONTACTAME</h3>
           <ContactInput placeholder={'Nombre'} inputValue={inputsFields.name} handleChange={(value: string) => { handleChangeInput('name', value) }}/>
           <ContactInput placeholder={'Email'} inputValue={inputsFields.email} handleChange={(value: string) => { handleChangeInput('email', value) }}/>
@@ -28,7 +28,7 @@ export function Contact (): JSX.Element {
             ENVIAR
           </button>
         </form>
-        <aside className='text-slate-200 font-bold bg-gradient-to-b from-slate-900 via-sky-900 to-sky-800 bg-opacity-20 flex justify-around flex-wrap p-1 gap-2 rounded-bl-md'>
+        <aside className='text-slate-200 font-bold bg-gradient-to-b from-slate-900 via-sky-900 to-sky-800 bg-opacity-20 flex items-center justify-around flex-wrap p-1 gap-2 rounded-bl-md sm:flex-col sm:px-2 sm:bg-gradient-to-r sm:rounded-bl-none sm:rounded-tr-md'>
           <ContactInfo/>
         </aside>
       </section>
