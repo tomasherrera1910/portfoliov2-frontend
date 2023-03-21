45f1b383eb33aefad9d271d821bec1f3397761e8
tomasherrera1910 <tomymza10@gmail.com>
2023-03-21 12:10:33 -0300

    using custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
