d4dbc8f44c2ba93016e07500d1e23009fdb7042b
tomasherrera1910 <tomymza10@gmail.com>
2023-03-23 19:14:51 -0300

    icons color

--- components/Contact/ContactInfo.tsx
+++ components/Contact/ContactInfo.tsx
@@ -4,9 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function ContactInfo (): JSX.Element {
   return (
         <>
-        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faEnvelope} width={16} className='text-slate-400'/> tomymza10@gmail.com</p>
-        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faLocationDot} width={16} className='text-slate-400'/> Mendoza, Argentina</p>
-        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faPhone} width={16} className='text-slate-400'/> +54 9 261 711-6355</p>
+        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faEnvelope} width={16} className='text-sky-600'/> tomymza10@gmail.com</p>
+        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faLocationDot} width={16} className='text-sky-600'/> Mendoza, Argentina</p>
+        <p className='flex flex-col items-center'><FontAwesomeIcon icon={faPhone} width={16} className='text-sky-600'/> +54 9 261 711-6355</p>
         </>
   )
 }
