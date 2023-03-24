ba9e6a83789bdb3a90b09cc44df4ea9cd3b5e1fe
tomasherrera1910 <tomymza10@gmail.com>
2023-03-24 17:22:36 -0300

    import google fonts in _document file how next documentation recommend

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document () {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;600&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
