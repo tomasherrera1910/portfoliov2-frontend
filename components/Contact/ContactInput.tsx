9e609fbdeee8cd55bdd002b709108168cbd112bf
tomasherrera1910 <tomymza10@gmail.com>
2023-04-03 13:03:41 -0300

    if input contain some and dont have focus blue color is darker

--- components/Contact/ContactInput.tsx
+++ components/Contact/ContactInput.tsx
@@ -17,13 +17,13 @@ export default function ContactInput ({ placeholder, inputValue, handleChange, a
   }
   return (
         <div className='relative my-6 md:my-0 md:mb-6'>
-            <label className={`text-neutral-500 absolute font-barlow top-0 left-0 py-1 px-2 duration-200 ease-in-out ${focus.input ? '-top-5 text-xs' : ''} ${focus.label ? 'text-sky-700' : ''}`} htmlFor={inputId}>
+            <label className={`text-neutral-500 absolute font-barlow top-0 left-0 py-1 px-2 duration-200 ease-in-out ${focus.input ? '-top-5 text-xs' : ''} ${focus.label ? 'text-sky-500' : ''} ${focus.input && !focus.label ? 'text-sky-900' : ''}`} htmlFor={inputId}>
                 {placeholder}
             </label>
             {area === true
               ? <textarea
             id={inputId}
-            className={'py-1 px-2 min-h-[80px] w-full h-fit bg-black bg-opacity-70 rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-700'}
+            className={`py-1 px-2 min-h-[80px] w-full h-fit bg-black bg-opacity-70 rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-500 ${focus.input && !focus.label ? 'border-b-sky-900' : ''}`}
             onFocus={handleFocus}
             onBlur={handleBlur}
             value={inputValue}
@@ -33,7 +33,7 @@ export default function ContactInput ({ placeholder, inputValue, handleChange, a
               : <input
             type='text'
             id={inputId}
-            className={'py-1 px-2 bg-black bg-opacity-70 w-full rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-700'}
+            className={`py-1 px-2 bg-black bg-opacity-70 w-full rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-500 ${focus.input && !focus.label ? 'border-b-sky-900' : ''}`}
             onFocus={handleFocus}
             onBlur={handleBlur}
             value={inputValue}
