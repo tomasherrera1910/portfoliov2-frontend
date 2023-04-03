f786533f8218a1b6a689712bc437e5515291cc35
tomasherrera1910 <tomymza10@gmail.com>
2023-04-03 13:00:19 -0300

    change input color if not focus

--- components/Contact/ContactInput.tsx
+++ components/Contact/ContactInput.tsx
@@ -8,22 +8,22 @@ interface Props {
 }
 export default function ContactInput ({ placeholder, inputValue, handleChange, area }: Props): JSX.Element {
   const inputId = useId()
-  const [focus, setFocus] = useState(false)
+  const [focus, setFocus] = useState({ label: false, input: false })
   const handleFocus = () => {
-    setFocus(true)
+    setFocus({ label: true, input: true })
   }
   const handleBlur = () => {
-    if (inputValue === '') { setFocus(false) }
+    if (inputValue === '') { setFocus({ label: false, input: false }) } else { setFocus({ label: false, input: true }) }
   }
   return (
         <div className='relative my-6 md:my-0 md:mb-6'>
-            <label className={`text-neutral-500 absolute font-barlow top-0 left-0 py-1 px-2 duration-200 ease-in-out ${focus ? 'text-sky-700 -top-5 text-xs' : ''}`} htmlFor={inputId}>
+            <label className={`text-neutral-500 absolute font-barlow top-0 left-0 py-1 px-2 duration-200 ease-in-out ${focus.input ? '-top-5 text-xs' : ''} ${focus.label ? 'text-sky-700' : ''}`} htmlFor={inputId}>
                 {placeholder}
             </label>
             {area === true
               ? <textarea
             id={inputId}
-            className={`py-1 px-2 min-h-[80px] w-full h-fit bg-black bg-opacity-70 rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear ${focus ? 'border-b-sky-700' : ''}`}
+            className={'py-1 px-2 min-h-[80px] w-full h-fit bg-black bg-opacity-70 rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-700'}
             onFocus={handleFocus}
             onBlur={handleBlur}
             value={inputValue}
@@ -33,7 +33,7 @@ export default function ContactInput ({ placeholder, inputValue, handleChange, a
               : <input
             type='text'
             id={inputId}
-            className={`py-1 px-2 bg-black bg-opacity-70 w-full rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear ${focus ? 'border-b-sky-700' : ''}`}
+            className={'py-1 px-2 bg-black bg-opacity-70 w-full rounded outline-none border-b-2 border-b-neutral-500 duration-300 ease-linear focus:border-b-sky-700'}
             onFocus={handleFocus}
             onBlur={handleBlur}
             value={inputValue}
