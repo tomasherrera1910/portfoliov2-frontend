8b464a90aea466c9c77de0cf26f0a1ebb18c07c5
tomasherrera1910 <tomymza10@gmail.com>
2023-04-03 15:21:04 -0300

    education array

---
+++ utils/education.ts
@@ -0,0 +1,20 @@
+import { EducationPlace } from './types'
+
+export const education: EducationPlace[] = [
+  {
+    id: 1,
+    name: 'UTN - Tecnicatura superior en Programación',
+    date: '2019-2022',
+    image: '/utn-logo.jpg',
+    url: 'https://www4.frm.utn.edu.ar/tecnicatura-superior-programacion/',
+    description: 'Este fue mi primer contacto con la programación, donde aprendí todas las bases de la misma y ademas lenguajes tales como Java, C#, C++, Javascript, bases de datos SQL y noSQL.'
+  },
+  {
+    id: 2,
+    name: 'NO COUNTRY',
+    date: '2023-Actualidad',
+    image: '/nocountry-logo.jpg',
+    url: 'https://www.nocountry.tech/',
+    description: 'Acá realizo simulaciones de entornos laborales, ya que No Country lo que hace es juntar un grupo de Juniors y ponerlos a hacer un proyecto grupal. Y esto me sirvió muchísimo para mejorar en mi trabajo en equipo. Actualmente llevo realizados 2 proyectos.'
+  }
+]
