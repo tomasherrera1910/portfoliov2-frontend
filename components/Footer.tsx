1c5f7d91ac1b6a2b93f6f124fec6edfb19b0f0d4
tomasherrera1910 <tomymza10@gmail.com>
2023-04-06 20:20:11 -0300

    change bg and hidden socials in xl screen

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Footer (): JSX.Element {
   return (
-    <footer className='bg-[#111] text-white py-2 flex flex-col items-center font-roboto'>
+    <footer className='bg-[#010108] border-t-[1px] border-t-slate-800 text-white py-2 flex flex-col items-center font-roboto'>
       <section className='flex max-w-3xl items-center justify-center'>
         <div className='flex flex-col gap-2'>
           <p className='flex text-sm gap-1 text-gray-300 items-center justify-center flex-wrap'>Designed and developed by
             <a href='https://www.linkedin.com/in/mauro-tomas-herrera/' target='_blank' rel='noreferrer' className='text-white font-bold text-base hover:underline'>Tomás Herrera</a>
             <span className='font-barlow font-semibold text-xs'>2022-2023</span>
           </p>
-          <nav className='flex justify-around items-center'>
+          <nav className='flex justify-around items-center xl:hidden'>
             <a
               href='https://www.linkedin.com/in/mauro-tomas-herrera/' target='_blank' rel='noreferrer'
               className='border-[1px] rounded-full p-2 duration-300 ease-in-out cursor-pointer bg-blue-600 text-white border-white grayscale hover:grayscale-0 hover:scale-110'
