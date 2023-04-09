ad0209c106d6c5e3e6f6d26471643df2015a0099
tomasherrera1910 <tomymza10@gmail.com>
2023-04-09 10:10:06 -0300

    add details in project card

--- components/Projects/ProjectHeader.tsx
+++ components/Projects/ProjectHeader.tsx
@@ -5,8 +5,8 @@ interface Props {
 export default function ProjectHeader ({ name, description }: Props): JSX.Element {
   return (
     <section>
-      <h3 className='font-semibold text-3xl'>{name}</h3>
-      <p className='max-w-sm opacity-80 text-sm'>{description}</p>
+      <h3 className='font-roboto font-semibold text-4xl'>{name}</h3>
+      <p className='font-barlow font-extralight max-w-sm opacity-80 text-sm'>{description}</p>
     </section>
   )
 }
