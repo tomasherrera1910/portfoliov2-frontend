b2e106a9331c9cca14e362317851d21bb9c2e832
tomasherrera1910 <tomymza10@gmail.com>
2023-04-12 12:44:10 -0300

    Navbar and home more gap and icons bigger

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -9,8 +9,8 @@ export function Navbar (): JSX.Element {
   const navMobile = `bg-black text-white bg-opacity-80 w-full duration-300 ease-in-out absolute z-30 top-0 h-screen xl:h-auto xl:h-calc(100vh - 80px) ${toggle ? 'left-0' : '-left-full'} flex flex-col items-center justify-center gap-8`
 
   return (
-    <div className='fixed flex z-30 w-full xl:relative xl:w-40'>
-      <aside className='bg-transparent flex z-30 items-center px-3 py-1 bg-opacity-80 w-full fixed xl:w-auto xl:sticky xl:top-0 xl:max-h-screen'>
+    <div className='fixed flex z-30 w-full xl:relative xl:w-60'>
+      <aside className='bg-transparent flex z-30 items-center px-3 py-1 bg-opacity-80 w-full fixed xl:w-full xl:sticky xl:top-0 xl:max-h-screen'>
         <button
           className='text-sky-500 absolute top-3 left-3 z-40 bg-slate-800 bg-opacity-75 border-[1px] border-slate-500 rounded-full p-2 ease-in-out duration-200 hover:scale-110 xl:hidden'
           onClick={handleToggle}
--- components/SocialsMedia.tsx
+++ components/SocialsMedia.tsx
@@ -8,17 +8,17 @@ export default function SocialsMedia (): JSX.Element {
     <>
       <a href='https://www.linkedin.com/in/mauro-tomas-herrera/' target='_blank' rel='noreferrer'>
         <li className='p-2 border-[1px] border-slate-300 rounded-full ease-in-out duration-300 hover:scale-125 text-blue-600 hover:text-slate-300 hover:bg-blue-600'>
-          <FontAwesomeIcon icon={faLinkedinIn as IconProp} className='w-9 h-9 xl:w-7 xl:h-7' />
+          <FontAwesomeIcon icon={faLinkedinIn as IconProp} className='w-9 h-9' />
         </li>
       </a>
       <a href='https://www.github.com/tomasherrera1910' target='_blank' rel='noreferrer'>
         <li className='p-2 border-[1px] border-slate-300 rounded-full ease-in-out duration-300 hover:scale-125 text-slate-300 hover:text-black hover:bg-slate-300'>
-          <FontAwesomeIcon icon={faGithub as IconProp} className='w-9 h-9 xl:w-7 xl:h-7' />
+          <FontAwesomeIcon icon={faGithub as IconProp} className='w-9 h-9' />
         </li>
       </a>
       <a href='https://www.github.com/tomasherrera1910' target='_blank' rel='noreferrer'>
         <li className='p-2 border-[1px] border-slate-300 rounded-full ease-in-out duration-300 hover:scale-125 text-red-800 hover:text-slate-300 hover:bg-red-800'>
-          <FontAwesomeIcon icon={faFileArrowDown as IconProp} className='w-9 h-9 xl:w-7 xl:h-7' />
+          <FontAwesomeIcon icon={faFileArrowDown as IconProp} className='w-9 h-9' />
         </li>
       </a>
     </>
