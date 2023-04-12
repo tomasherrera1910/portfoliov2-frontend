a1f1b58ebb148b47dd9444c6906b83859e52a598
tomasherrera1910 <tomymza10@gmail.com>
2023-04-12 12:44:34 -0300

    Contact section styles

--- components/Contact/ContactInfo.tsx
+++ components/Contact/ContactInfo.tsx
@@ -2,11 +2,12 @@ import { faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function ContactInfo (): JSX.Element {
+  const pStyle = 'flex flex-col items-center text-sm sm:text-base lg:flex-row gap-2'
   return (
     <>
-      <p className='flex flex-col items-center'><FontAwesomeIcon icon={faEnvelope} width={16} className='text-sky-600' /> tomymza10@gmail.com</p>
-      <p className='flex flex-col items-center'><FontAwesomeIcon icon={faLocationDot} width={16} className='text-sky-600' /> Mendoza, Argentina</p>
-      <p className='flex flex-col items-center'><FontAwesomeIcon icon={faPhone} width={16} className='text-sky-600' /> +54 9 261 711-6355</p>
+      <p className={`${pStyle}`}><FontAwesomeIcon icon={faEnvelope} className='text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> tomymza10@gmail.com</p>
+      <p className={`${pStyle}`}><FontAwesomeIcon icon={faLocationDot} className='text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> Mendoza, Argentina</p>
+      <p className={`${pStyle}`}><FontAwesomeIcon icon={faPhone} className='text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> +54 9 261 711-6355</p>
     </>
   )
 }
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -17,22 +17,26 @@ export function Contact (): JSX.Element {
     })
   }
   return (
-    <section className='flex flex-col sm:flex-row sm:justify-center z-10'>
-      <form className='bg-black bg-opacity-20 rounded-tr-md p-2 sm:grow sm:rounded-bl-md sm:rounded-tr-none max-w-sm'>
-        <h3 className='text-sky-600 font-roboto font-bold text-3xl sm:text-5xl text-center animate-pulse'>CONTACTAME</h3>
-        <section className='md:flex md:gap-2 md:mt-6'>
-          <ContactInput placeholder='Nombre' inputValue={inputsFields.name} handleChange={(value: string) => { handleChangeInput('name', value) }} />
-          <ContactInput placeholder='Email' inputValue={inputsFields.email} handleChange={(value: string) => { handleChangeInput('email', value) }} />
-        </section>
-        <ContactInput placeholder='Asunto' inputValue={inputsFields.subject} handleChange={(value: string) => { handleChangeInput('subject', value) }} />
-        <ContactInput area placeholder='Mensaje' inputValue={inputsFields.message} handleChange={(value: string) => { handleChangeInput('message', value) }} />
-        <button className='bg-black font-roboto bg-opacity-70 w-full py-2 rounded-sm border-b-2 border-b-slate-400 text-slate-400 ease-linear duration-200 hover:text-sky-900 hover:border-b-sky-900 hover:scale-95'>
-          ENVIAR
-        </button>
-      </form>
-      <aside className='text-slate-200 font-roboto font-medium bg-black bg-opacity-70 border-t-[1px] border-t-sky-600 sm:border-t-0 sm:border-l-[1px] sm:border-l-sky-600 max-w-sm flex items-center justify-around flex-wrap p-1 gap-2 rounded-bl-md sm:flex-col sm:px-2 sm:rounded-bl-none sm:rounded-tr-md'>
-        <ContactInfo />
-      </aside>
+    <section className='w-full flex flex-col max-w-5xl items-center sm:items-start z-10 my-12'>
+      <h3 className='text-sky-600 font-roboto font-bold text-4xl sm:text-5xl animate-pulse'>CONTACTAME</h3>
+      <section className='w-full flex flex-col max-w-5xl items-center sm:flex-row sm:justify-start'>
+        <form className='bg-black w-full bg-opacity-20 rounded-tr-md p-2 sm:grow sm:rounded-bl-md sm:rounded-tr-none max-w-sm sm:max-w-lg'>
+          <div className='px-2'>
+            <section className='md:flex md:mt-6 md:justify-between'>
+              <ContactInput placeholder='Nombre' inputValue={inputsFields.name} handleChange={(value: string) => { handleChangeInput('name', value) }} />
+              <ContactInput placeholder='Email' inputValue={inputsFields.email} handleChange={(value: string) => { handleChangeInput('email', value) }} />
+            </section>
+            <ContactInput placeholder='Asunto' inputValue={inputsFields.subject} handleChange={(value: string) => { handleChangeInput('subject', value) }} />
+            <ContactInput area placeholder='Mensaje' inputValue={inputsFields.message} handleChange={(value: string) => { handleChangeInput('message', value) }} />
+            <button className='bg-black font-roboto bg-opacity-70 w-full py-2 rounded-sm border-b-2 border-b-slate-400 text-slate-400 ease-linear duration-200 hover:text-sky-900 hover:border-b-sky-900 hover:scale-95'>
+              ENVIAR
+            </button>
+          </div>
+        </form>
+        <aside className='text-slate-200 font-roboto font-medium bg-black bg-opacity-70 border-t-[1px] border-t-sky-600 sm:border-t-0 sm:border-l-[1px] sm:border-l-sky-600 max-w-sm h-full flex items-center justify-around flex-wrap p-1 gap-2 rounded-bl-md sm:flex-col sm:px-2 sm:rounded-bl-none sm:rounded-tr-md lg:px-4'>
+          <ContactInfo />
+        </aside>
+      </section>
     </section>
   )
 }
