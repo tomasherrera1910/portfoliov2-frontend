27e55adef02bd6dae8e86be9b3799371d31c11bb
tomasherrera1910 <tomymza10@gmail.com>
2023-04-20 20:24:39 -0300

    some animations

--- components/Contact/ContactInfo.tsx
+++ components/Contact/ContactInfo.tsx
@@ -5,9 +5,9 @@ export default function ContactInfo (): JSX.Element {
   const pStyle = 'flex flex-col items-center text-sm sm:text-base lg:flex-row gap-2'
   return (
     <>
-      <p className={`${pStyle}`}><FontAwesomeIcon icon={faEnvelope} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> tomymza10@gmail.com</p>
-      <p className={`${pStyle}`}><FontAwesomeIcon icon={faLocationDot} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> Mendoza, Argentina</p>
-      <p className={`${pStyle}`}><FontAwesomeIcon icon={faPhone} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> +54 9 261 711-6355</p>
+      <p data-aos='flip-up' className={`${pStyle}`}><FontAwesomeIcon icon={faEnvelope} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> tomymza10@gmail.com</p>
+      <p data-aos='flip-up' className={`${pStyle}`}><FontAwesomeIcon icon={faLocationDot} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> Mendoza, Argentina</p>
+      <p data-aos='flip-up' className={`${pStyle}`}><FontAwesomeIcon icon={faPhone} className='text-blue-800 dark:text-sky-600 w-4 h-4 lg:w-6 lg:h-6' /> +54 9 261 711-6355</p>
     </>
   )
 }
--- components/Education/EducationCard.tsx
+++ components/Education/EducationCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function EducationCard ({ place }: Props): JSX.Element {
   const { toggle, handleToggle } = useToggle()
   return (
-    <li className='flex gap-2 items-center border-l-[1px] border-l-sky-800 pl-2 duration-200 ease-linear hover:border-l-sky-500'>
+    <li data-aos='fade-right' className='flex gap-2 sm:gap-8 items-center border-l-[1px] border-l-sky-800 pl-2 duration-200 ease-linear hover:border-l-sky-500'>
       <img src={place.image} alt={`${place.name} LOGO`} className='rounded-full object-cover w-20 h-20 lg:w-28 lg:h-28' />
       <aside className='flex flex-col max-w-sm lg:max-w-2xl'>
         <a href={place.url} target='_blank' rel='noreferrer'>
