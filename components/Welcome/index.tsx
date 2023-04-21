b81bb469856f5c7971d977307c4d31653ae45b02
tomasherrera1910 <tomymza10@gmail.com>
2023-04-21 13:53:45 -0300

    tranlating welcome component

--- components/Welcome/index.tsx
+++ components/Welcome/index.tsx
@@ -3,10 +3,11 @@ import SocialsMedia from '../SocialsMedia'
 import { useContext } from 'react'
 import { SidebarContext } from '../../context/sidebar'
 import { OptionsButtons } from '../OptionsButtons'
+import useI18N from '../../hooks/useI18N'
 
 export function Welcome (): JSX.Element {
   const { aboutMeRef } = useContext(SidebarContext)
-
+  const { t } = useI18N()
   return (
     <section className='flex relative items-center h-screen w-full font-roboto justify-center' ref={aboutMeRef} id='welcome'>
       <OptionsButtons />
@@ -16,7 +17,7 @@ export function Welcome (): JSX.Element {
         <ul className='flex justify-around w-72 py-4 border-b-[1px] border-b-slate-700 xl:hidden'>
           <SocialsMedia />
         </ul>
-        <p className='font-barlow dark:font-light font-semibold text-center text-slate-500 dark:text-slate-400 max-w-sm sm:text-lg md:text-xl'>Tengo 21 años, soy Técnico superior en Programación y un desarrollador web autodidacta, estoy listo para el salto laboral.</p>
+        <p className='font-barlow dark:font-light font-semibold text-center text-slate-500 dark:text-slate-400 max-w-sm sm:text-lg md:text-xl'>{t('aboutMe')}</p>
       </aside>
       <picture className='hidden lg:block drop-shadow-3xl-dark dark:drop-shadow-3xl-light'>
         <img src='/foto.png' alt='Mi foto' className='w-96' />
