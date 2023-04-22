69794bcefb68007aa6dcb5b11332e42fa41c0b44
tomasherrera1910 <tomymza10@gmail.com>
2023-04-22 19:38:20 -0300

    missing translation

--- components/Projects/ProjectTechnologies.tsx
+++ components/Projects/ProjectTechnologies.tsx
@@ -1,12 +1,14 @@
+import useI18N from '../../hooks/useI18N'
 import { Skill } from '../../utils/types'
 
 interface Props {
   technologies: Skill[]
 }
 export default function ProjectTechnologies ({ technologies }: Props): JSX.Element {
+  const { t } = useI18N()
   return (
     <section className='my-3 max-w-xs'>
-      <h4>Tecnologías utilizadas:</h4>
+      <h4 className='font-roboto'>{t('projectsTechnologies')}</h4>
       <ul className='flex w-full gap-x-4 flex-wrap'>
         {technologies.map((technology) => (
           <li key={technology.id} className='flex items-center gap-2'>
