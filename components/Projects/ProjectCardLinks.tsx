7918f49202215e8b8f77a485864fda12476be53f
tomasherrera1910 <tomymza10@gmail.com>
2023-04-27 12:36:58 -0300

    change links styles

--- components/Projects/ProjectCardLinks.tsx
+++ components/Projects/ProjectCardLinks.tsx
@@ -12,20 +12,21 @@ interface Props {
   secondary: string
 }
 export default function ProjectCardLinks ({ backendRepo, frontEndRepo, deployURL, primary, secondary }: Props): JSX.Element {
+  const LinkBaseStyle = 'flex gap-2 bg-slate-500 dark:bg-slate-900 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 dark:hover:bg-slate-500 dark:hover:text-black hover:bg-slate-700 hover:text-white hover:scale-105'
   return (
-    <footer data-aos='flip-up' className='flex flex-col gap-2 justify-around items-center sm:mt-2 sm:flex-row sm:grow'>
+    <footer data-aos='flip-up' className='w-full flex gap-2 justify-around items-center sm:mt-2 flex-wrap sm:grow'>
       {(backendRepo != null) &&
-        <Link href={backendRepo} className='flex gap-2 bg-slate-500 dark:bg-slate-900 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105'>
+        <Link href={backendRepo} className={`${LinkBaseStyle}`}>
           <span>Backend</span>
           <FontAwesomeIcon icon={faGithub as IconProp} width={20} />
         </Link>}
       {(frontEndRepo != null) &&
-        <Link href={frontEndRepo} className='flex gap-2 bg-slate-500 dark:bg-slate-900 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105'>
+        <Link href={frontEndRepo} className={`${LinkBaseStyle}`}>
           <span>Frontend</span>
           <FontAwesomeIcon icon={faGithub as IconProp} width={20} />
         </Link>}
       {(deployURL != null) &&
-        <Link href={deployURL} className='flex gap-2 bg-slate-500 dark:bg-slate-900 items-center rounded p-1 w-fit bg-opacity-50 shadow-sm ease-linear duration-150 hover:scale-105'>
+        <Link href={deployURL} className={`${LinkBaseStyle}`}>
           <span>Deploy</span>
           <FontAwesomeIcon icon={faLink} width={20} />
         </Link>}
